b90d2a474e366401e064d133d6d30a5cf5ffe644
fidgetboyrolly <fidgetboyrolly@icloud.com>
2025-04-17 09:51:58 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,30 +1,26 @@
-const CACHE_NAME = 'search-engine-cache-v1';
+const CACHE_NAME = 'fiddlelooo-cache-v1';
 const urlsToCache = [
     '/',
     '/index.html',
     '/style.css',
     '/search.js',
-    '/sites.txt',
-    '/manifest.json',
-    '/icon-192x192.png',
-    '/icon-512x512.png'
+    '/Icon-192x192.jpg',
+    '/Icon-512x512.jpg'
 ];
 
 self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                return cache.addAll(urlsToCache);
-            })
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(urlsToCache);
+        })
     );
 });
 
 self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                return response || fetch(event.request);
-            })
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
     );
 });
 
@@ -34,7 +30,7 @@ self.addEventListener('activate', event => {
         caches.keys().then(cacheNames => {
             return Promise.all(
                 cacheNames.map(cacheName => {
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                    if (!cacheWhitelist.includes(cacheName)) {
                         return caches.delete(cacheName);
                     }
                 })
